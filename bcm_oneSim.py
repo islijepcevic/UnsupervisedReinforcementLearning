f0f3f7d6bc49726063971a40f1473aef22091aa8
Deniz Aydin <deniz.aydin@epfl.ch>
2013-11-20 19:25:04 +0100

    bcm_onSim.py is now more correct than before! Inputs presented randomly at each time step rather than being constant for each simulation.

--- bcm_oneSim.py
+++ bcm_oneSim.py
@@ -5,7 +5,7 @@ import copy
 random.seed(0)
 
 NUM_NEURONS = 100
-TIME_LIMIT = 50000
+TIME_LIMIT = 100000
 MU_RANGE = 5
 NUM_EXPTS = 30
 eta = 5.0*(10**(-2))
@@ -41,16 +41,16 @@ for experiment in range(NUM_EXPTS): #simulate the paradigm using many different 
 	
 		x = [constant*math.exp(-(float(i+1)-mu_j)**2/sigma**2) for i in range(NUM_NEURONS)]
 		#print "init x: ",x
-		y.append(sum([wi*xi for wi,xi in zip(w_0,x)]))
+		y = sum([wi*xi for wi,xi in zip(w_0,x)])
 		#print "init y: ",y
-		y2.append(y[j]**2)
+		y2 = y**2
 
 	
 		# print "time: ",t			
 		for neuron in range(NUM_NEURONS):
 		#	print "neuron:",neuron
-			dtheta=  (1.0/tau)*(y2[j]-theta_0[j])
-			dw = eta*x[neuron]*(y2[j]-(theta_0[j]*y[j]))
+			dtheta=  (1.0/tau)*(y2-theta_0[j])
+			dw = eta*x[neuron]*(y2-(theta_0[j]*y))
 		#	print "delta_theta: ",dtheta, " delta_w: " , dw
 			w_0[neuron] +=  dw 
 			if w_0[neuron] < 0:
@@ -61,9 +61,9 @@ for experiment in range(NUM_EXPTS): #simulate the paradigm using many different 
 			if (t % 1000) == 0:
 				print "time: ",t	
 	
-		y[j] = sum([wi*xi for wi,xi in zip(w_0,x)])
+		y = sum([wi*xi for wi,xi in zip(w_0,x)])
 		#print "after all neurons updated by dt, y: ",y		
-		y2[j] = y[j]**2
+		y2= y**2
 
 	for i in range(NUM_NEURONS):
 		f.write(str("%lf\n"%w_0[i]))
