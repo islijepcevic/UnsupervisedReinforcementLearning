9cf6176c4b3da3d859fab89e84f8ba69de3190cc
islijepcevic <islijepcevic@gmail.com>
2013-11-22 01:56:23 +0100

    some commit for enabling pull

--- miniproject1/ex2/plot_fixedMu_allExpts.py
+++ miniproject1/ex2/plot_fixedMu_allExpts.py
@@ -2,20 +2,18 @@ import matplotlib
 import matplotlib.pyplot as plt 
 import math
 import pylab 
-TIME_LIMIT = 50000
-NUM_EXPTS = 30
-NUM_INPUTS = 5 # num of input patterns
+import cst_sim as cs
 
-for oneInput in range(NUM_INPUTS):
-	data = [[float(line)  for line in open("weights%d_expt%d_t%d" %(oneInput , i,TIME_LIMIT))] for i in range(NUM_EXPTS)] 
-	#load data in data[expt][neuron] format for given sim length
-	colors = ['red', 'green', 'blue', 'yellow',  'black']
-	fig = pylab.figure()
-	mean = [pylab.mean([data[expt][neuron] for expt in range(NUM_EXPTS)]) for neuron in range(len(data[0]))]
-	for expt in range(NUM_EXPTS):	
-		pylab.plot(mean, color='red',linewidth=1.0)
-		pylab.hold(True)
-	fig.suptitle('Mean response to $\mu_j$ = %d after t = %d, average of %d'%(20*oneInput+10,TIME_LIMIT,NUM_EXPTS), fontsize=14)
-	#pylab.legend(loc='upper left',prop={'size':10})
-	pylab.savefig("weights_mu%d_t%d_allExpts_mean_results.png"%(20*oneInput+10,TIME_LIMIT))
+data = [[float(line)  for line in open("weights_for_expt%d_t%d" %(i,cs.TIME_LIMIT))] for i in range(cs.NUM_EXPTS)] 
+#print len(data[0])
+#load data in data[expt][neuron] format for given sim length
+colors = ['red', 'green', 'blue', 'yellow',  'black']
+fig = pylab.figure()
+mean = [pylab.mean([data[expt][neuron] for expt in range(cs.NUM_EXPTS)]) for neuron in range(len(data[0]))]
+	
+pylab.plot(mean, color='red',linewidth=1.0)
+pylab.hold(True)
+fig.suptitle('Mean response after t = %d, average of %d'%(cs.TIME_LIMIT,cs.NUM_EXPTS), fontsize=14)
+#pylab.legend(loc='upper left',prop={'size':10})
+pylab.savefig("weights_t%d_mean_results.png"%(cs.TIME_LIMIT))
 
