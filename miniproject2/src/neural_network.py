a2e8872486cd9c61441e1b4d260dc40965431df2
islijepcevic <islijepcevic@gmail.com>
2013-12-09 15:11:57 +0100

    code for the neural network

--- miniproject2/src/neural_network.py
+++ miniproject2/src/neural_network.py
@@ -9,41 +9,86 @@ number of output neurons also
 
     def __init__(self, pneurons, pboundaries, vneurons, vboundaries, noutputs):
         """constructor
-        @param pneurons tuple (pair) of neurons per X and Y axis for
-            encoding position
-        @param pboundaries list of lists: for every neuron, the interval
-        @param vneurons tuple (pair) of neurons per X and Y axis for
-            encoding velocity
-        @param vboundaries list of lists: for every neuron, the interval
-        @param noutputs number of output neurons
+        @param pneurons number of neurons per axis for encoding position
+        @param pboundaries the interval
+        @param vneurons number of neurons per axis for encoding velocity
+        @param vboundaries the interval
+        @param noutputs number of output neurons (should be 9 to be compatible
+            with track.py)
         """
 
         # these are "centers" of neuron encodings
-        self.xPositions = np.linspace(pboundaries[0][0], pboundaries[0][1], pneurons[0])
-        self.yPositions = np.linspace(pboundaries[1][0], pboundaries[1][1], pneurons[1])
-        self.xVelocities = np.linspace(vboundaries[0][0], vboundaries[0][1], vneurons[0])
-        self.yVelocities = np.linspace(vboundaries[1][0], vboundaries[1][1], vneurons[1])
+        self.positions = np.linspace(pboundaries[0], pboundaries[1], pneurons)
+        self.velocities = np.linspace(vboundaries[0], vboundaries[1], vneurons)
 
-        self.npos = pneurons[0]*pneurons[1]
-        self.nvel = vneurons[0]*vneurons[1]
+        self.posDeviation = self.positions[1] - self.positions[0]
+        self.velDeviation = self.velocities[1] - self.velocities[0]
+
+        self.npos = pneurons*pneurons
+        self.nvel = vneurons*vneurons
         self.ntotal = self.npos + self.nvel
+
+        self.inputs = np.zeros(self.ntotal)
 
         self.noutputs = noutputs
 
         # list (for each action) of np arrays
         self.weights = []
         self.etraces = []
+        self.Qoutputs = []
         for i in xrange(noutputs):
             self.weigts.append( np.zeros(self.ntotal) )
             self.etraces.append( np.zeros(self.ntotal) )
+            self.Qoutputs.append(0.0)
 
     def reset(self):
         for i in xrange(len(self.etraces)):
             self.etraces[i] = np.zeros(self.ntotal)
 
-    def set_network_input(self):
-        pass
+    def _setNetworkInput(self, pos, vel):
+        """given current position and velocity, set the network's input values"""
+        for i in xrange(len(self.positions)):
+            for j in xrange(len(self.positions)):
 
-    def compute_network_output(self, x, y):
-        """computes Q values"""
-        pass
+                term1 = np.square(pos[0] - self.positions[i]) 
+                term2 = np.square(pos[1] - self.positions[j])
+                exponent = -(term1 + term2) / 2.0 / np.square(self.posDeviation)
+
+                index = i*len(self.positions) + j
+                self.inputs[index] = np.exp(exponent)
+
+        for i in xrange(len(self.velocities)):
+            for j in xrange(len(self.velocities)):
+
+                term1 = np.square(vel[0] - self.velocities[i]) 
+                term2 = np.square(vel[1] - self.velocities[j])
+                exponent = -(term1 + term2) / 2.0 / np.square(self.velDeviation)
+
+                index = self.npos + i*len(self.velocities) + j
+                self.inputs[index] = np.exp(exponent)
+
+
+    def computeNetworkOutput(self, pos, vel):
+        """computes Q values
+        returns Q values (but also stores them)
+        """
+        self._setNetworkInput(pos, vel)
+        for i in xrange(len(self.Qoutputs)):
+            self.Qoutputs[i] = np.dot(self.weights[i], self.inputs)
+
+        return self.Qoutputs
+
+    def getActionDirection(self, a):
+        """computes the direction for action a
+        @param a - integer, index to Q value list
+        """
+
+        if a == 0:
+            return [0.0, 0.0]
+
+        ndir = self.noutputs - 1
+
+        dirx = np.cos(-2.0*np.pi*a/ndir + np.pi/2.0)
+        diry = np.sin(-2.0*np.pi*a/ndir + np.pi/2.0)
+
+        return (dirx, diry)
